4a87e9f944af8567a6bb9bac0657df9a8fa428c9
Tim Ebert <tim.ebert@sap.com>
2020-10-14 10:00:08 +0200

    Add vendor files

---
+++ vendor/github.com/gardener/gardener/.github/ISSUE_TEMPLATE/bug.md
@@ -0,0 +1,38 @@
+---
+name: Bug Report
+about: Report a bug encountered while operating Gardener
+labels: kind/bug
+
+---
+
+**How to categorize this issue?**
+<!--
+Please select area, kind, and priority for this issue. This helps the community categorizing it.
+Replace below TODOs or exchange the existing identifiers with those that fit best in your opinion.
+If multiple identifiers make sense you can also state the commands multiple times, e.g.
+  /area control-plane
+  /area auto-scaling
+  ...
+
+"/area" identifiers:     audit-logging|auto-scaling|backup|certification|control-plane-migration|control-plane|cost|delivery|dev-productivity|disaster-recovery|documentation|high-availability|logging|metering|monitoring|networking|open-source|operations|ops-productivity|os|performance|quality|robustness|scalability|security|storage|testing|usability|user-management
+"/kind" identifiers:     api-change|bug|cleanup|discussion|enhancement|epic|impediment|poc|post-mortem|question|regression|task|technical-debt|test
+"/priority" identifiers: normal|critical|blocker
+-->
+/area TODO
+/kind bug
+/priority normal
+
+**What happened**:
+
+**What you expected to happen**:
+
+**How to reproduce it (as minimally and precisely as possible)**:
+
+**Anything else we need to know?**:
+
+**Environment**:
+
+- Gardener version:
+- Kubernetes version (use `kubectl version`):
+- Cloud provider or hardware configuration:
+- Others:
---
+++ vendor/github.com/gardener/gardener/pkg/utils/test/matchers/matchers.go
@@ -0,0 +1,70 @@
+// Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package matchers
+
+import (
+	"github.com/onsi/gomega/format"
+	"github.com/onsi/gomega/types"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+)
+
+// DeepEqual returns a Gomega matcher which checks whether the expected object is deeply equal with the object it is
+// being compared against.
+func DeepEqual(expected interface{}) types.GomegaMatcher {
+	// if CharactersAroundMismatchToInclude is too small, then format.MessageWithDiff will be unable to output our
+	// mismatch message
+	if format.CharactersAroundMismatchToInclude < 50 {
+		format.CharactersAroundMismatchToInclude = 50
+	}
+
+	return newDeepEqualMatcher(expected)
+}
+
+// DeepDerivativeEqual is similar to DeepEqual except that unset fields in actual are
+// ignored (not compared). This allows us to focus on the fields that matter to
+// the semantic comparison.
+func DeepDerivativeEqual(expected interface{}) types.GomegaMatcher {
+	// if CharactersAroundMismatchToInclude is too small, then format.MessageWithDiff will be unable to output our
+	// mismatch message
+	if format.CharactersAroundMismatchToInclude < 50 {
+		format.CharactersAroundMismatchToInclude = 50
+	}
+
+	return newDeepDerivativeMatcher(expected)
+}
+
+// BeNotFoundError checks if error is NotFound.
+func BeNotFoundError() types.GomegaMatcher {
+	return &kubernetesErrors{
+		checkFunc: apierrors.IsNotFound,
+		message:   "NotFound",
+	}
+}
+
+// BeForbiddenError checks if error is Forbidden.
+func BeForbiddenError() types.GomegaMatcher {
+	return &kubernetesErrors{
+		checkFunc: apierrors.IsForbidden,
+		message:   "Forbidden",
+	}
+}
+
+// BeBadRequestError checks if error is BadRequest.
+func BeBadRequestError() types.GomegaMatcher {
+	return &kubernetesErrors{
+		checkFunc: apierrors.IsBadRequest,
+		message:   "BadRequest",
+	}
+}
