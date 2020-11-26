f61e05ea301f0804f0750d1f41d868b38aa6e649
Tim Ebert <tim.ebert@sap.com>
2020-11-26 15:47:15 +0100

    Fix release test config/script

--- .ci/test-e2e.sh
+++ .ci/test-e2e.sh
@@ -10,6 +10,8 @@ mkdir -p /tm
 /cc/utils/cli.py config attribute --cfg-type kubernetes --cfg-name testmachinery --key kubeconfig > /tm/kubeconfig
 /testrunner run \
     --tm-kubeconfig-path=/tm/kubeconfig \
+    --no-execution-group \
+    --testrun-prefix terraformer-pod-e2e- \
     --timeout=1800 \
     --testruns-chart-path=.ci/testruns/default \
     --set revision="$(cat ./VERSION)"
