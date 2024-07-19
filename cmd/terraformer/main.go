40d2a86e49b8bfccec821fcc2356c86035728c11
Andrew Bond <a.bond@sap.com>
2024-07-19 16:18:05 -0500

    Add a network connectivity check to make sure pod can reach kubernetes API before executing terraform.

--- cmd/terraformer/main.go
+++ cmd/terraformer/main.go
@@ -7,8 +7,10 @@ package main
 import (
 	"errors"
 	"fmt"
+	"net/http"
 	"os"
 	"os/exec"
+	"time"
 
 	runtimelog "sigs.k8s.io/controller-runtime/pkg/log"
 
@@ -17,7 +19,39 @@ import (
 	"github.com/gardener/terraformer/pkg/utils"
 )
 
+func checkNetworkConnectivity(url string) error {
+	client := &http.Client{
+		Timeout: 5 * time.Second,
+	}
+
+	resp, err := client.Get(url)
+	if err != nil {
+		return fmt.Errorf("unable to reach the server: %v", err)
+	}
+	defer resp.Body.Close()
+
+	// Just return nil if the server is reachable, regardless of the status code
+	return nil
+}
+
 func main() {
+	// Check if the KUBERNETES_SERVICE_HOST environment variable is set
+	kubernetesServiceHost := os.Getenv("KUBERNETES_SERVICE_HOST")
+	if kubernetesServiceHost == "" {
+		panic("KUBERNETES_SERVICE_HOST is not set. cannot start terraformer.")
+	}
+
+	// Retry network connectivity check every 5 seconds until successful
+	url := fmt.Sprintf("https://%s", kubernetesServiceHost)
+	for {
+		if err := checkNetworkConnectivity(url); err != nil {
+			fmt.Printf("network connectivity check failed: %v. Retrying in 5 seconds...\n", err)
+			time.Sleep(5 * time.Second)
+			continue
+		}
+		break
+	}
+
 	if err := exec.Command("which", terraformer.TerraformBinary).Run(); err != nil {
 		panic("terraform is not installed or not executable. cannot start terraformer.")
 	}
